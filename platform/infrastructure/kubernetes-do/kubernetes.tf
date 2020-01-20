9fae4a4f823b105d07d67b5b8b37389452a83ab1
Jake Sanders <jake@micro.mu>
2020-01-20 18:35:38 +0000

    Digitalocean Kubernetes as code

---
+++ platform/infrastructure/kubernetes-do/kubernetes.tf
@@ -0,0 +1,48 @@
+data "digitalocean_kubernetes_versions" "k8s_versions" {
+  version_prefix = "${var.k8s_version}."
+}
+
+data "digitalocean_sizes" "valid_sizes" {
+  filter {
+    key    = "vcpus"
+    values = var.node_cpu
+  }
+
+  filter {
+    key    = "memory"
+    values = var.node_memory
+  }
+
+  filter {
+    key    = "regions"
+    values = [var.region]
+  }
+
+  sort {
+    key       = "price_monthly"
+    direction = "asc"
+  }
+}
+
+resource "random_id" "k8s_name" {
+  byte_length = 4
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s_cluster" {
+  name    = "micro-k8s-${var.region}-${random_id.k8s_name.hex}"
+  region  = var.region
+  version = data.digitalocean_kubernetes_versions.k8s_versions.latest_version
+
+  node_pool {
+    name       = "default-${random_id.k8s_name.hex}"
+    node_count = var.node_count
+
+    # This fails on apply if there were no valid sizes found
+    size = length(data.digitalocean_sizes.valid_sizes.sizes) > 0 ? element(data.digitalocean_sizes.valid_sizes.sizes, 0).slug : null
+  }
+}
+
+# Cluster name is output for later use with data sources
+output "cluster_name" {
+  value = digitalocean_kubernetes_cluster.k8s_cluster.name
+}
---
+++ platform/infrastructure/kubernetes-do/provider.tf
@@ -0,0 +1,14 @@
+# A DigitalOcean personal access token
+variable "do_token" {
+  description = "Your DigitalOcean personal access token"
+  default     = ""
+}
+
+provider "digitalocean" {
+  token   = var.do_token
+  version = "~> 1.12"
+}
+
+provider "random" {
+  version = "~> 2.2"
+}
